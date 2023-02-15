7229be7e57b2acf657c1a49d15e28615fddb8067
Vladlen <vladlenpolyakov+github@gmail.com>
2023-02-16 00:18:01 +0300

    test: Add tests for endpoints

--- internal/usecase/country.go
+++ internal/usecase/country.go
@@ -9,44 +9,12 @@ type CountryUC struct {
 }
 
 // ChangeCountry changes country of fetched trends in chat.
-func (uc *CountryUC) ChangeCountry(id int, text string) error {
-	country, err := uc.validateCountry(text)
-	if err != nil {
-		return fmt.Errorf("usecase - ChangeCountry: %w", err)
-	}
-
+func (uc *CountryUC) ChangeCountry(id int, country string) error {
 	if err := uc.repo.ChangeCountry(id, country); err != nil {
 		return fmt.Errorf("usecase - ChangeCountry: %w", err)
 	}
 
 	return nil
-}
-
-// TODO: Switch to map after increasing the number of countries
-
-// validateCountry converts emoji to plain text and returns
-// an error if there is no reference.
-func (uc *CountryUC) validateCountry(text string) (string, error) {
-	switch text {
-	case "🇩🇪":
-		return "DE", nil
-	case "🇪🇸":
-		return "ES", nil
-	case "🇫🇷":
-		return "FR", nil
-	case "🇮🇹":
-		return "IT", nil
-	case "🇬🇧":
-		return "GB", nil
-	case "🇷🇺":
-		return "RU", nil
-	case "🇺🇦":
-		return "UA", nil
-	case "🇺🇸", "🇺🇲":
-		return "US", nil
-	default:
-		return "", errInvalidCountry
-	}
 }
 
 func newCountryUC(repo CountryRepo) *CountryUC {
